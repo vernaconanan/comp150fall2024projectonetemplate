31cda477709d001a8382a3d72adde8bd40391eda
ajm10565 <ajm10565@gmail.com>
2024-10-29 17:00:38 -0500

    condensed the work from this past week

--- extra1.py
+++ extra1.py
@@ -1,42 +1,34 @@
-# Extra file to run code separately.
-# more scenarios and gaming option.
+import time
+import random
 
-# Settings: initial setting is in a digital arcade game of "wreck it ralph" as the game progress the setting can change into different kinds of arcade games where various characters will be offered and players will select who they will play to win the level. Depending upon the setting this will determine the level's difficulty!!
-    # Final boss levels, glitches or bugs will be the unexpected obstacles players occur, and possible crossovers with characters/ abilities/ and settings.
+class Character:
+    def __init__(self, name, vitality, experience=0, level=1):
+        self.name = name
+        self.vitality = vitality
+        self.experience = experience
+        self.level = level
+        self.powerups = 0
 
-# party: additional characters that can be added/ earned once certain milestone levels are passed:
-    # unique characteristics are encouraged for each character.
+    def level_up(self):
+        self.level += 1
+        self.vitality += 5
+        print(f"{self.name} leveled up to level {self.level}! Vitality is now {self.vitality}.")
 
-# For leveling up new characters can be unlocked as well as coins/ or points added to the players characters. Or possiblilty of when they level up special abilities can be unlocked for a limited time for specific characters'.
-    ## Possible code class for leveling up: *COPILOT* added characters could be villians.
-def level_up(self):
-    self.level += 1
-    self.strength += 1
-    self.dexterity += 1
-    self.constitution += 1
-    self.vitality += 1
-    self.endurance += 1
-    self.intelligence += 1
-    self.wisdom += 1
-    self.knowledge += 1
-    self.willpower += 1
-    self.spirit += 1
-    self.capacity += 1
-    print(f"{self.name} leveled up to level {self.level}!")
+    def break_obstacle(self):
+        print(f"{self.name} is breaking an obstacle...")
+        time.sleep(1)
+        print(f"{self.name} broke the obstacle!\n")
 
-## Ralph and Fleix would get plugged in here:
-    ralph = Character("Ralph", 10, 5, 8, 12, 7, 4, 3, 6, 9, 5, 10)
-    ralph.level_up()
-
-## Possible code for un-eventful scenarios:
-# import random
+    def gain_powerup(self):
+        self.powerups += 1
+        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}")
 
 class Event:
     def __init__(self, description, choices):
-       self.description = description
-       self.choices = choices
+        self.description = description
+        self.choices = choices
 
-def trigger_event(self, character):
+    def trigger_event(self, character):
         print(self.description)
         for i, choice in enumerate(self.choices):
             print(f"{i + 1}. {choice['description']}")
@@ -44,7 +36,7 @@ def trigger_event(self, character):
         outcome = self.choices[choice]['outcome']
         self.resolve_outcome(character, outcome)
 
-def resolve_outcome(self, character, outcome):
+    def resolve_outcome(self, character, outcome):
         success = random.choice([True, False])  # Simplified success check
         if success:
             print(outcome['success'])
@@ -52,331 +44,82 @@ def resolve_outcome(self, character, outcome):
         else:
             print(outcome['failure'])
             character.vitality -= outcome['damage']
+        if character.experience >= 20:
+            character.level_up()
+            character.experience = 0
 
-### Scenario example:
-event = Event(
-   "A glitch in the system causes chaos!",
-    [
-        {"description": "Use Dexterity to navigate through.", "outcome": {"success": "You navigated successfully!", "failure": "You got hurt!", "experience": 10, "damage": 2}},
-        {"description": "Use Intelligence to stabilize the system.", "outcome": {"success": "You stabilized the system!", "failure": "It didn't work!", "experience": 15, "damage": 3}},
-    ]
-)
-
-event.trigger_event(Ralph)
-
-def game_loop():
-    ralph = Character("Ralph", 10, 5, 8, 12, 7, 4, 3, 6, 9, 5, 10)
-    events = [
-        Event(
-            "A glitch in the system causes chaos!",
-            [
-                {"description": "Use Dexterity to navigate through.", "outcome": {"success": "You navigated successfully!", "failure": "You got hurt!", "experience": 10, "damage": 2}},
-                {"description": "Use Intelligence to stabilize the system.", "outcome": {"success": "You stabilized the system!", "failure": "It didn't work!", "experience": 15, "damage": 3}},
-            ]
-        )
-    ]
-
-    while ralph.vitality > 0:
-        event = random.choice(events)
-        event.trigger_event(ralph)
-        if ralph.experience >= 20:
-            ralph.level_up()
-            ralph.experience = 0
-
-    print("Game Over")
-
-import time
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.powerups = 0
-    def break_obstacle(self):
-        print(f"{self.name} is breaking an obstacle...")
-        time.sleep(1)  # Simulate time taken to break
-        print(f"{self.name} broke the obstacle!\n")
-    def gain_powerup(self):
-        self.powerups += 1
-        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}\n")
 class Game:
     def __init__(self):
-        self.ralph = Character("Ralph")
+        self.ralph = Character("Ralph", vitality=20)
+        self.inventory = Inventory()
+
     def start_level_1(self):
         print("Welcome to Level 1: The Tutorial")
-        print("A virus is infecting the game, turning characters into monsters.")
-        print("It's up to Ralph to save them!\n")
-        
-        input("Press Enter to continue...")
-        
-        # Introduce basic mechanics
-        print("Ralph learns how to break obstacles!")
-        time.sleep(1)
-        
-        # Simulate breaking the first obstacle
         self.ralph.break_obstacle()
-        
-        print("Ralph sees an infected character!")
-        input("Press Enter to attempt to save them...")
-        
-        # Simulate another action
-        self.ralph.break_obstacle()
-        
-        print("Ralph has saved the infected character!")
         self.ralph.gain_powerup()
-        
-        print("Tutorial complete! You have learned the basics of gameplay.")
-        print("Now you can move on to the next level.\n")
-# Start the game LEVEL 1:
-if __name__ == "__main__":
-    game = Game()
-    game.start_level_1()
-import time
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.powerups = 0
-    def break_obstacle(self):
-        print(f"{self.name} is breaking an obstacle...")
-        time.sleep(1)  # Simulate time taken to break
-        print(f"{self.name} broke the obstacle!\n")
-    def gain_powerup(self):
-        self.powerups += 1
-        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}\n")
-class Game:
-    def __init__(self):
-        self.ralph = Character("Ralph")
-        self.infected_characters = 5  # Number of characters to save
+
     def start_level_2(self):
         print("Welcome to Level 2: Freeing Felix")
-        print("Ralph needs to break free non-infected characters to find Felix.\n")
-        while self.infected_characters > 0:
+        infected_characters = 5
+        while infected_characters > 0:
             action = input("Press 'b' to break an obstacle or 's' to skip: ").lower()
             if action == 'b':
                 self.ralph.break_obstacle()
-                self.infected_characters -= 1
-                print(f"Ralph saved a non-infected character! {self.infected_characters} left to save.\n")
-                self.ralph.gain_powerup()  # Gain a power-up for each character saved
+                infected_characters -= 1
+                self.ralph.gain_powerup()
             elif action == 's':
-                print("Ralph is searching the area for clues...")
+                print("Searching for obstacles...")
                 time.sleep(1)
-                print("Ralph finds some debris that can be broken!\n")
-                self.ralph.break_obstacle()
-            else:
-                print("Invalid input! Please press 'b' to break or 's' to skip.")
-        print("All non-infected characters have been freed!")
-        print("Ralph has located Felix! Time to move to the next level.\n")
-# Start the game LEVEL 2:
-if __name__ == "__main__":
-    game = Game()
-    game.start_level_2()
-import time
-import random
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.powerups = 0
-    def break_obstacle(self):
-        print(f"{self.name} is breaking an obstacle...")
-        time.sleep(1)  # Simulate time taken to break
-        print(f"{self.name} broke the obstacle!\n")
-    def gain_powerup(self):
-        self.powerups += 1
-        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}\n")
-class Game:
-    def __init__(self):
-        self.ralph = Character("Ralph")
-        self.debris_count = 5  # Number of debris to break
-        self.powerups_found = 0
+
     def start_level_3(self):
         print("Welcome to Level 3: Finding the Back Staircase")
-        print("Ralph needs to break through debris to find the staircase leading to Felix.\n")
-        while self.debris_count > 0:
+        debris_count = 5
+        while debris_count > 0:
             action = input("Press 'b' to break an obstacle or 's' to search for power-ups: ").lower()
             if action == 'b':
                 self.ralph.break_obstacle()
-                self.debris_count -= 1
-                self.ralph.gain_powerup()  # Gain a power-up for each debris broken
-                print(f"Debris left to break: {self.debris_count}\n")
+                debris_count -= 1
+                self.ralph.gain_powerup()
             elif action == 's':
-                found = random.choice([True, False])
-                if found:
-                    self.powerups_found += 1
-                    print(f"{self.ralph.name} found a hidden power-up! Total power-ups found: {self.powerups_found}\n")
-                else:
-                    print("No power-ups found this time.\n")
-            else:
-                print("Invalid input! Please press 'b' to break or 's' to search.")
-        print("Ralph has cleared the path and found the back staircase!")
-        print("Time to advance to Level 4.\n")
-# Start the game LEVEL 3:
-if __name__ == "__main__":
-    game = Game()
-    game.start_level_3()
-import time
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.powerups = 0
-    def break_obstacle(self):
-        print(f"{self.name} is breaking an obstacle...")
-        time.sleep(1)  # Simulate time taken to break
-        print(f"{self.name} broke the obstacle!\n")
-    def gain_powerup(self):
-        self.powerups += 1
-        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}\n")
-class Game:
-    def __init__(self):
-        self.ralph = Character("Ralph")
-        self.felix_locked = True  # Indicates if Felix is locked
-        self.bars_broken = 0
-        self.total_bars = 3  # Number of bars to break to unlock Felix
+                print("Searching for power-ups...")
+                time.sleep(1)
+
     def start_level_4(self):
         print("Welcome to Level 4: Unlocking Felix")
-        print("Ralph needs to break the bars to free Felix.\n")
-        while self.felix_locked:
+        bars_broken = 0
+        total_bars = 3
+        while bars_broken < total_bars:
             action = input("Press 'b' to break a bar or 's' to check surroundings: ").lower()
             if action == 'b':
-                if self.bars_broken < self.total_bars:
-                    self.ralph.break_obstacle()
-                    self.bars_broken += 1
-                    print(f"Bars left to break: {self.total_bars - self.bars_broken}\n")
-                    if self.bars_broken == self.total_bars:
-                        self.felix_locked = False  # Unlock Felix
-                        print("Felix is now free!")
-                        print("Felix reveals that his hammer has been taken. We need to find it!\n")
-                else:
-                    print("All bars are already broken!\n")
-            elif action == 's':
-                print("Ralph searches the area but finds nothing useful.\n")
-            else:
-                print("Invalid input! Please press 'b' to break or 's' to search.")
-        print("Ralph and Felix will now search for the hammer in the next level!\n")
-# Start the game LEVEL 4:
-if __name__ == "__main__":
-    game = Game()
-    game.start_level_4()
-import time
-import random
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.powerups = 0
-    def break_obstacle(self):
-        print(f"{self.name} is breaking an obstacle...")
-        time.sleep(1)  # Simulate time taken to break
-        print(f"{self.name} broke the obstacle!\n")
-    def gain_powerup(self):
-        self.powerups += 1
-        print(f"{self.name} gained a power-up! Total power-ups: {self.powerups}\n")
-class Game:
-    def __init__(self):
-        self.ralph = Character("Ralph")
-        self.felix = Character("Fix-It Felix")
-        self.puzzles_solved = 0
-        self.total_puzzles = 3  # Number of puzzles to solve
+                self.ralph.break_obstacle()
+                bars_broken += 1
+
     def start_level_5(self):
         print("Welcome to Level 5: Solving Puzzles with Felix")
-        print("Felix must solve puzzles to find his hammer while Ralph breaks obstacles.\n")
-        while self.puzzles_solved < self.total_puzzles:
-            action = input("Press 'p' to solve a puzzle or 'b' to have Ralph break an obstacle: ").lower()
+        puzzles_solved = 0
+        total_puzzles = 3
+        while puzzles_solved < total_puzzles:
+            action = input("Press 'p' to solve a puzzle or 'b' to break an obstacle: ").lower()
             if action == 'p':
-                self.solve_puzzle()
-            elif action == 'b':
-                self.ralph.break_obstacle()
-            else:
-                print("Invalid input! Please press 'p' to solve a puzzle or 'b' to break an obstacle.")
-        print("Felix has solved all the puzzles and located his hammer!")
-        print("Ralph and Felix are ready to take on the next challenge!\n")
-    def solve_puzzle(self):
-        # Simulating a simple puzzle-solving mechanic
-        print(f"{self.felix.name} is attempting to solve a puzzle...")
-        time.sleep(1)  # Simulate time taken to solve
-        success = random.choice([True, False])  # Random success/failure
-        if success:
-            self.puzzles_solved += 1
-            print(f"{self.felix.name} solved a puzzle! Puzzles solved: {self.puzzles_solved}/{self.total_puzzles}\n")
-            self.felix.gain_powerup()  # Gain a power-up for each puzzle solved
-        else:
-            print(f"{self.felix.name} failed to solve the puzzle. Try again!\n")
-# Start the game
-if __name__ == "__main__":
-    game = Game()
-    game.start_level_5()
-#### Felix is solving puzzles and Ralph is assisting while wrecking obtascles.
+                puzzles_solved += 1
+                print(f"Solved puzzle {puzzles_solved} of {total_puzzles}")
 
-### LEVEL 6 COPILOT CODE ** Puzzle **
-#import random
-#import time
+    def run(self):
+        levels = [self.start_level_1, self.start_level_2, self.start_level_3, self.start_level_4, self.start_level_5]
+        for level in levels:
+            level()
 
-class GearPuzzle:
-   def __init__(self):
-        self.gear_sequence = [random.randint(1, 5) for _ in range(5)]  # Random gear sequence
-        self.player_sequence = [0] * 5  # Player's current sequence
-
-def display_puzzle(self):
-    print("Current Gear Sequence: ", self.player_sequence)
-    
-def input_move(self, index, value):
-    if 0 <= index < len(self.gear_sequence):
-        self.player_sequence[index] = value
-    
-def check_solution(self):
-    return self.player_sequence == self.gear_sequence
-
-class Enemy:
-    def __init__(self, name, health):
-        self.name = name
-        self.health = health
-    
-    def attack(self):
-        print(f"{self.name} is attacking!")
-    
-    def take_damage(self, damage):
-        self.health -= damage
-        if self.health <= 0:
-            print(f"{self.name} has been defeated!")
-
-def main():
-    puzzle = GearPuzzle()
-    enemies = [Enemy("Minion", 10) for _ in range(5)]  # Initial set of enemies
-
-    while not puzzle.check_solution():
-        puzzle.display_puzzle()
-        
-        # Felix's turn to make a move
-        index = int(input("Enter gear index (0-4): "))
-        value = int(input("Enter gear value (1-5): "))
-        puzzle.input_move(index, value)
-
-        # Ralph's turn to defend
-        for enemy in enemies:
-            if enemy.health > 0:
-                enemy.attack()
-                time.sleep(1)  # Simulate time taken for Ralph to react
-                enemy.take_damage(10)  # Ralph's attack damage
-                
-        print("\n")
-
-    print("Puzzle Solved! Door Unlocked.")
-
-if __name__ == "__main__":
-    main()
-
-# Extra file to run code separately. 
 class Inventory:
     def __init__(self):
         self.items = {}
 
     def add_item(self, item_name, quantity):
-        if item_name in self.items:
-            self.items[item_name] += quantity
-        else:
-            self.items[item_name] = quantity
+        self.items[item_name] = self.items.get(item_name, 0) + quantity
         print(f"Added {quantity} {item_name}(s) to your inventory.")
 
     def use_item(self, item_name, character):
-        if item_name in self.items and self.items[item_name] > 0:
+        if self.items.get(item_name, 0) > 0:
             print(f"{character.name} uses {item_name}.")
-            # Define item effects, e.g., healing:
             if item_name == "Health Potion":
                 character.vitality += 10
                 print(f"{character.name} regained 10 vitality points!")
@@ -384,6 +127,6 @@ class Inventory:
         else:
             print(f"No {item_name} left in inventory.")
 
-inventory = Inventory()
-inventory.add_item("Health Potion", 3)
-inventory.use_item("Health Potion", ralph)
+# Initialize and run the game
+game = Game()
+game.run()
